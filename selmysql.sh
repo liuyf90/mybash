d10a7cd3f544a8cfb29a5ffffc3897279fe8b167
liuyf90 <liuyf90@gmail.com>
2021-12-23 16:55:51 +0800

    add select mysql servers script

--- selmysql.sh
+++ selmysql.sh
@@ -2,6 +2,7 @@
 #通过配置~/.mysqlcnf目录下的.cnf文件来增加多个mysql的配置
 select mysql in  `ls -a ~/.mysqlcnf | grep .cnf` ;
 do [[$mysql]] 
-      mysql --defaults-extra-file=~/.mysqlcnf/$mysql  ;
+      MYSQLC=$mysql  && mysql --defaults-extra-file=~/.mysqlcnf/$mysql  ;
+
       break;
   done
